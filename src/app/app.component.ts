7dfa7a648e95387086c05f847f2af40d382b1970
Jim <jim.b@hotmail.com>
2021-03-26 16:29:26 -0400

    Updated linter and fixed files. Added linter rules for converting to 4 spaces and ran fixes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'v-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: "v-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'vector';
+    title = "vector";
 }
