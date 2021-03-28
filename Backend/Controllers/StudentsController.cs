c7d1e932bf616aa89580f8499f3985def261e9a0
Jim <jim.b@hotmail.com>
2021-03-28 15:31:03 -0400

    Added students controller and models

---
+++ Backend/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VectorSolution.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Student
+            {
+                FirstName = "test",
+                MiddleName = "test",
+                LastName = "test",
+                EmailAddress = "test",
+                Id = 1
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Backend/Models/Student.cs
@@ -0,0 +1,8 @@
+public class Student
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+}
